af32e5c206ac17109f510f92f9d5e13f693fc5c7
getb1 <guyetb1@gmail.com>
2024-11-22 20:12:07 +0000

    Started work on parsing FENs

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-
+#include <string.h>
 typedef unsigned long long int U64;
 
 #define BOARD_SIZE 64
@@ -22,6 +22,10 @@ int toggle_bit(int pos, int number) {
     return (number ^ (1 << (pos)));
 }
 
+int coordinates_to_number(int row, int col) {
+    return (8*row)+col;
+}
+
 /* Defnition of the boar type and its relating funcions */
 typedef struct BOARD {
     U64 WHITE;
@@ -32,6 +36,12 @@ typedef struct BOARD {
     U64 BISHOPS;
     U64 QUEENS;
     U64 KINGS;
+
+    int enPassantsq;
+    int moves;
+    int turn;
+    int halfMoveCLock;
+    int castleFlags;
 } board_t;
 
 board_t * init_board() {
@@ -48,10 +58,6 @@ board_t * init_board() {
     new->KINGS = 0x0800000000000008;
 
     return new;
-}
-
-int coordinates_to_number(int row, int col) {
-    return (8*row)+col;
 }
 
 char get_piece_at_square(board_t * board, int square) {
@@ -98,13 +104,76 @@ void display_board(board_t * board) {
 
 }
 
+
+board_t * init_from_FEN(char fen[]) {
+    board_t * board = NULL;
+    board = (board_t *) malloc(sizeof(board_t));
+    board->WHITE = 0x0000000000000000;
+    board->BLACK = 0x0000000000000000;
+    board->BISHOPS = 0x0000000000000000;
+    board->PAWNS = 0x0000000000000000;
+    board->ROOKS = 0x0000000000000000;
+    board->KNIGHTS = 0x0000000000000000;
+    board->QUEENS = 0x0000000000000000;
+    board->KINGS = 0x0000000000000000;
+    
+    char * token = strtok(fen, " ");
+
+
+    char * pieces = token;
+    token = strtok(NULL, " ");
+    char * turn = token;
+    token = strtok(NULL, " ");
+    char * castle = token;
+    token = strtok(NULL, " ");
+    char * enpass = token;
+    token = strtok(NULL, " ");
+    char * halfmove_clock = token;
+    token = strtok(NULL, " ");
+    char * moves = token;
+    
+    
+    
+
+    token = strtok(pieces,"/");
+    
+    for(int i=0;i<8;++i) {
+        int pos =0;
+        for(int j=0;j<sizeof(token);++i) {
+            if(isalpha(token[j])) {
+                int sq=coordinates_to_number(i,j+pos);
+                if(isupper(token[j])) {
+                    board->WHITE = set_bit(sq,board->WHITE, 1);
+                } else{
+                    board->BLACK = set_bit(sq,board->BLACK, 1);
+                }
+
+                if(strncmp(tolower(token[j]),"p")) {
+                    board->PAWNS = set_bit(sq,board->PAWNS,1);
+                } else if(strncmp(tolower(token[j])=="r")) {
+                    board->ROOKS = set_bit(sq,board->PAWNS,1);
+                }
+                
+
+            }
+        }
+        token =(NULL,"/");
+    }
+
+
+    display_board(board);
+}
+
+
+
+
+
 int main() {
 
     board_t * the_board = init_board();
-
-    //printf("%c",get_piece_at_square(the_board, 40));
-    //printf("%d",get_bit(the_board->ROOKS, 56));
-    display_board(the_board);
+    char fen[] = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+    
+    init_from_FEN(fen);
     
     return 0;
 }
